620f4ee60eba93a8b2dbe1bf2be47d28da1aea81
Will Bamford <will.bamford@twinkl.co.uk>
2024-07-08 15:18:35 +0100

    changes

--- apps/www/contentlayer.config.js
+++ apps/www/contentlayer.config.js
@@ -10,10 +10,6 @@ import rehypeSlug from "rehype-slug"
 import { codeImport } from "remark-code-import"
 import remarkGfm from "remark-gfm"
 import { getHighlighter, loadTheme } from "shiki"
-import { visit } from "unist-util-visit"
-
-import { rehypeComponent } from "./lib/rehype-component"
-import { rehypeNpmCommand } from "./lib/rehype-npm-command"
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
@@ -74,78 +70,67 @@ export default makeSource({
   documentTypes: [Doc],
   mdx: {
     remarkPlugins: [remarkGfm, codeImport],
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeComponent,
-      () => (tree) => {
-        visit(tree, (node) => {
-          if (node?.type === "element" && node?.tagName === "pre") {
-            const [codeEl] = node.children
-            if (codeEl.tagName !== "code") {
-              return
-            }
+    // rehypePlugins: [
+    //   rehypeSlug,
+    //   rehypeComponent,
+    //   () => (tree) => {
+    //   },
+    //   [
+    //     rehypePrettyCode,
+    //     {
+    //       getHighlighter: async () => {
+    //         const theme = await loadTheme(
+    //           path.join(process.cwd(), "lib/vscode-theme.json")
+    //         )
+    //         return await getHighlighter({ theme })
+    //       },
+    //       onVisitLine(node) {
+    //         // Prevent lines from collapsing in `display: grid` mode, and allow empty
+    //         // lines to be copy/pasted
+    //         if (node.children.length === 0) {
+    //           node.children = [{ type: "text", value: " " }]
+    //         }
+    //       },
+    //       onVisitHighlightedLine(node) {
+    //         node.properties.className.push("line--highlighted")
+    //       },
+    //       onVisitHighlightedWord(node) {
+    //         node.properties.className = ["word--highlighted"]
+    //       },
+    //     },
+    //   ],
+    //   () => (tree) => {
+    //     visit(tree, (node) => {
+    //       if (node?.type === "element" && node?.tagName === "div") {
+    //         if (!("data-rehype-pretty-code-fragment" in node.properties)) {
+    //           return
+    //         }
 
-            node.__rawString__ = codeEl.children?.[0].value
-            node.__src__ = node.properties?.__src__
-          }
-        })
-      },
-      [
-        rehypePrettyCode,
-        {
-          getHighlighter: async () => {
-            const theme = await loadTheme(
-              path.join(process.cwd(), "lib/vscode-theme.json")
-            )
-            return await getHighlighter({ theme })
-          },
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }]
-            }
-          },
-          onVisitHighlightedLine(node) {
-            node.properties.className.push("line--highlighted")
-          },
-          onVisitHighlightedWord(node) {
-            node.properties.className = ["word--highlighted"]
-          },
-        },
-      ],
-      () => (tree) => {
-        visit(tree, (node) => {
-          if (node?.type === "element" && node?.tagName === "div") {
-            if (!("data-rehype-pretty-code-fragment" in node.properties)) {
-              return
-            }
+    //         const preElement = node.children.at(-1)
+    //         if (preElement.tagName !== "pre") {
+    //           return
+    //         }
 
-            const preElement = node.children.at(-1)
-            if (preElement.tagName !== "pre") {
-              return
-            }
+    //         preElement.properties["__withMeta__"] =
+    //           node.children.at(0).tagName === "div"
+    //         preElement.properties["__rawString__"] = node.__rawString__
 
-            preElement.properties["__withMeta__"] =
-              node.children.at(0).tagName === "div"
-            preElement.properties["__rawString__"] = node.__rawString__
-
-            if (node.__src__) {
-              preElement.properties["__src__"] = node.__src__
-            }
-          }
-        })
-      },
-      rehypeNpmCommand,
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            className: ["subheading-anchor"],
-            ariaLabel: "Link to section",
-          },
-        },
-      ],
-    ],
+    //         if (node.__src__) {
+    //           preElement.properties["__src__"] = node.__src__
+    //         }
+    //       }
+    //     })
+    //   },
+    //   rehypeNpmCommand,
+    //   [
+    //     rehypeAutolinkHeadings,
+    //     {
+    //       properties: {
+    //         className: ["subheading-anchor"],
+    //         ariaLabel: "Link to section",
+    //       },
+    //     },
+    //   ],
+    // ],
   },
 })
